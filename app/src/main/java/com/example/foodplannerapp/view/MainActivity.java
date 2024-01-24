4d9c52aa0bff9165fc965dbecd7d688561d2a8d6
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-01-24 16:01:15 +0200

    init: initialize project

---
+++ app/src/main/java/com/example/foodplannerapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodplannerapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodplannerapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
